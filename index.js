3963bc84ddae88b66cc7c6dd43a3b2ab4178ccd5
Administrator <admin@example.com>
2021-10-20 12:09:49 +0200

    2. Habilita sintaxis de imports

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
@@ -6,6 +6,18 @@ const app = express();
 //Definir puerto
 
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {//req: request, res: response
+    res.send('Hello World');
+});
+
+app.get('/nosotros', (req, res) => {//req: request, res: response
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => {//req: request, res: response
+    res.send('Contacto');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
