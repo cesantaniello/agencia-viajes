af6c78b891ae2d3fd29ea9930ab7c9ce658cc9a5
Administrator <admin@example.com>
2021-10-20 15:54:11 +0200

    9. Middleware en Express

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const port = process.env.PORT || 4000;
 //Habilitar PUG
 app.set('view engine', 'pug');
 
+//Obtener año actual
+app.use((req,res,next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    next();
+});
+
 //Definir la carpeta publica
 app.use(express.static('public'))
 
