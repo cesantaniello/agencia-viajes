2dc95ac9f9a01ac9fdee034fde4129fd39ab455b
Administrator <admin@example.com>
2021-10-20 13:04:52 +0200

    5. Pasa variables a vistas

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {//req: request, res: response
-    res.send('Hello World');
+    res.render('inicio');
 });
 
 router.get('/nosotros', (req, res) => {//req: request, res: response
+    const viajes = 'Viaje a Alemania';
     res.render('nosotros');
 });
 
