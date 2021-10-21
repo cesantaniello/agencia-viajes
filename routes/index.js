a741a51e77aba85da6cc49b5fdf8de2a87c94d89
Administrator <admin@example.com>
2021-10-21 11:24:13 +0200

    15. Crea un Controlador

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,14 @@
 import express from 'express';
+import { paginaInicio, paginaNosotros, paginaViajes, paginaTestimoniales } from '../controllers/paginasController.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {//req: request, res: response
-    res.render('inicio', {
-        pagina: 'Inicio'
-    });
-});
+router.get('/', paginaInicio);
 
-router.get('/nosotros', (req, res) => {
-    res.render('nosotros', {
-        pagina: 'Nosotros'
-    });
-});
+router.get('/nosotros', paginaNosotros);
 
-router.get('/viajes', (req, res) => {
-    res.render('viajes', {
-        pagina: 'Viajes'
-    });
-});
+router.get('/viajes', paginaViajes);
 
-router.get('/testimoniales', (req, res) => {
-    res.render('testimoniales', {
-        pagina: 'Testimoniales'
-    });
-});
+router.get('/testimoniales', paginaTestimoniales);
 
 export default router;
