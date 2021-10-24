6ba782329e670bb249e2d0f0df7c2abdccf21c18
Administrator <admin@example.com>
2021-10-24 20:21:10 +0200

    22. Consulta los Testimoniales en la Base de Datos

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,4 +1,5 @@
 import {Viaje} from '../models/Viaje.js';
+import {Testimonial} from '../models/Testimoniales.js';
 
 const paginaInicio = (req, res) => {
     res.render('inicio', {
@@ -24,10 +25,17 @@ const paginaViajes =  async (req, res) => {
     });
 }
 
-const paginaTestimoniales = (req, res) => {
-    res.render('testimoniales', {
-        pagina: 'Testimoniales'
-    });
+const paginaTestimoniales = async(req, res) => {
+    
+    try {
+        const testimoniales = await Testimonial.findAll();
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            testimoniales
+        });
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 //Muestra un viaje por su slug
