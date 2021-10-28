72fc7c2f4ccc1b11ec1dc9db16cd509681248a54
Administrator <admin@example.com>
2021-10-28 17:59:41 +0200

    29. Agrega variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
 
-const db = new Sequelize('agenciaviajes', 'root', '', {
-    host: '127.0.0.1',
-    port: 3307,
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
