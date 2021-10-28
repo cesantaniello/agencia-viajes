b83f58f9b3771f9bdf05e033dbe6487d7e47af9b
Administrator <admin@example.com>
2021-10-28 21:04:30 +0200

    32. Reconfigura variables de entorno para despliegue

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config({path:"variables.env"});
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
-    host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
+const db = new Sequelize('agenciaviajes', 'root', '', {
+    host: '127.0.0.1',
+    port: 3307,
     dialect: 'mysql',
     define: {
         timestamps: false
